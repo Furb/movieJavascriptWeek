013248632299054540884c32d71a171ee86f81f1
Ruth Moritz Mikkelsen <eaarumi@students.eaaa.dk>
2020-11-20 12:06:57 +0100

    help from Torill

--- movies.js
+++ movies.js
@@ -1,36 +1,36 @@
-var movie = [ 
-{
-    "Title": "Willow",
-	"YouTubeId": "v=uzn2izehkno",
-	"API": "http://www.omdbapi.com/?t=Willow&apikey=dab96338"
-},
+var movies = [
+	{
+		"Title": "Willow",
+		"YouTubeId": "v=uzn2izehkno",
+		"API": "http://www.omdbapi.com/?t=Willow&apikey=dab96338"
+	},
 
-{
-	"Title": "Up",
-	"YouTubeId": "ORFWdXl_zJ4",
-	"API": "http://www.omdbapi.com/?t=Up&apikey=dab96338"
-},
-{
-	"Title": "Lord of the Rings",
-	"YouTubeID": "V75dMMIW2B4 ",
-	"API": "http://www.omdbapi.com/?t=Lord+of+the+rings&apikey=dab96338"
-},
- {
-	"Title": "The princess bride",
-	"YouTubeId": "WNNUcHRiPS8",
-	"API": "http://www.omdbapi.com/?t=The+princess+bride&apikey=dab96338"
-	
-},
- {
-	"Title": "Alice in Wonderland",
-	"YouTubeId": "P_IVeP3i7B0",
-	"API": "http://www.omdbapi.com/?t=Alice+in+wonderland&apikey=dab96338"
-},
- {
-	"Title": "Indiana Jones",
-	"YouTubeId": "v=Rh_BJXG1-44",
-	"API": "http://www.omdbapi.com/?t=Indiana+jones&apikey=dab96338"
-}]
+	{
+		"Title": "Up",
+		"YouTubeId": "ORFWdXl_zJ4",
+		"API": "http://www.omdbapi.com/?t=Up&apikey=dab96338"
+	},
+	{
+		"Title": "The Lord of the Rings: The Fellowship of the Ring",
+		"YouTubeId": "V75dMMIW2B4 ",
+		"API": "http://www.omdbapi.com/?t=Lord+of+the+rings&apikey=dab96338"
+	},
+	{
+		"Title": "The Princess Bride",
+		"YouTubeId": "WNNUcHRiPS8",
+		"API": "http://www.omdbapi.com/?t=The+princess+bride&apikey=dab96338"
+
+	},
+	{
+		"Title": "Alice in Wonderland",
+		"YouTubeId": "P_IVeP3i7B0",
+		"API": "http://www.omdbapi.com/?t=Alice+in+wonderland&apikey=dab96338"
+	},
+	{
+		"Title": "Indiana Jones and the Last Crusade",
+		"YouTubeId": "v=Rh_BJXG1-44",
+		"API": "http://www.omdbapi.com/?t=Indiana+jones&apikey=dab96338"
+	}]
 
 /*
 
@@ -69,35 +69,36 @@ container.setAttribute('class', 'container');
 //attaching the logo and the container to the root element
 //app.appendChild(logo);
 app.appendChild(container);
-const app = document.getElementById("root")
 
-movie.forEach(movie => {
+
+movies.forEach(movie => {
 	const video = document.createElement('div');
 	video.setAttribute('class', 'trailer');
 
 	//add title to the movie
 	const h2 = document.createElement('h2');
-	h2.textContent = movies.Title
+	h2.textContent = movie.Title
 
+	let div=document.createElement('div');
+	div.setAttribute("id",movie.Title);
+	let htmldiv=document.getElementById('movieInfo');
+	htmldiv.appendChild(div);
+	//get from API
+	apiUrl = movie.API;
+	fetch(apiUrl)
 
-apiUrl = movies.API;
-fetch(apiUrl).then( response => { return Response.json(); })
-.then(data => { data.forEach(thisMovie => {
-  const h3 = document.createElement('h3');
-  h3.textContent = thisMovie.Plot;
-});
-	video.appendChild(h2);
-
-		
+		.then(response => { return response.json(); })
+		.then(data => {
+			const h3 = document.createElement('h3');
+			h3.textContent = data.Title;
+			let div=document.getElementById(data.Title);
+			div.appendChild(h3);
 		})
 
-
-	
 		.catch(err => {
 			// Do something for an error here
 			const errorMessage = document.createElement('marquee');
 			errorMessage.textContent = `Gah, it's not working!`;
 			app.appendChild(errorMessage);
 		})
-	});
-	 
+});
